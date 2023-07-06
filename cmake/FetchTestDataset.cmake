578d54d3d48b267c298d839325bca9cfea107588
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-07-06 19:30:31 +0200

    Update test dataset URL

--- cmake/FetchTestDataset.cmake
+++ cmake/FetchTestDataset.cmake
@@ -4,7 +4,7 @@
 
 # cmake-format: off
 file(
-  DOWNLOAD https://www.dropbox.com/s/l6rymg9mezixin6/hictk_test_data.tar.xz?dl=1
+  DOWNLOAD https://zenodo.org/record/8121687/files/hictk_test_data.tar.xz?download=1
   EXPECTED_HASH SHA256=4ac996d0169410036aa1c8a0aeb49da9c44a44cf47b255eb589650adc5ea3468
   ${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.xz)
 # cmake-format: on
