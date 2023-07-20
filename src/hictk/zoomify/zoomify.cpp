c0c12787465b7833c0afa7c3983ec83ff1134b3f
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-07-20 11:50:10 +0200

    Update hictk zoomify

--- src/hictk/zoomify/zoomify.cpp
+++ src/hictk/zoomify/zoomify.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 #include "hictk/cooler/cooler.hpp"
+#include "hictk/cooler/multires_cooler.hpp"
 #include "hictk/cooler/utils.hpp"
 #include "hictk/tmpdir.hpp"
 #include "hictk/tools/config.hpp"
@@ -32,55 +33,47 @@ internal::PixelMerger<CoarsenIt> setup_pixel_merger(const cooler::File& clr, std
   return {heads, tails};
 }
 
-[[nodiscard]] static std::uint32_t compute_base_resolution(
-    const std::vector<std::uint32_t>& resolutions, std::uint32_t target_res) {
-  assert(!resolutions.empty());
-  assert(target_res < resolutions.front());
-  assert(target_res % resolutions.front() == 0);
+void zoomify_once(const cooler::File& clr1, cooler::RootGroup entrypoint2,
+                  std::uint32_t resolution) {
+  auto clr2 = cooler::File::create(entrypoint2, clr1.chromosomes(), resolution);
 
-  return *std::find_if(resolutions.rbegin(), resolutions.rend(),
-                       [&](const auto res) { return res < target_res && res % target_res == 0; });
+  std::vector<ThinPixel<std::int32_t>> buffer{500'000};
+  cooler::MultiResFile::coarsen(clr1, clr2, buffer);
 }
 
-static void coarsen_cooler(std::string input_uri, std::string output_uri, std::uint32_t resolution,
-                           std::vector<ThinPixel<std::int32_t>>& buffer) {
-  auto clr1 = cooler::File::open_read_once(input_uri);
-  auto clr2 = cooler::File::create(output_uri, clr1.chromosomes(), resolution);
+void zoomify_once(std::string_view uri1, std::string_view uri2, std::uint32_t resolution,
+                  bool force) {
+  auto clr1 = cooler::File::open(uri1);
 
-  auto sel1 = clr1.fetch();
-  auto sel2 = transformers::CoarsenPixels(sel1.begin<std::int32_t>(), sel1.end<std::int32_t>(),
-                                          clr1.bins_ptr(), clr2.bin_size() / clr1.bin_size());
+  spdlog::info(FMT_STRING("coarsening cooler at {} once ({} -> {})"), clr1.uri(), clr1.bin_size(),
+               resolution);
 
-  const auto update_frequency =
-      std::max(std::size_t(1'000'000), (clr1.dataset("pixels/bin1_id").size() / 100));
+  auto mode = force ? HighFive::File::Overwrite : HighFive::File::Create;
+  cooler::RootGroup entrypoint2{HighFive::File(std::string{uri2}, mode).getGroup("/")};
 
-  auto first = sel2.begin();
-  auto last = sel2.end();
-  buffer.clear();
+  return zoomify_once(clr1, entrypoint2, resolution);
+}
 
-  auto t0 = std::chrono::steady_clock::now();
-  for (std::size_t j = 0; first != last; ++j) {
-    buffer.emplace_back(*first);
-    if (buffer.size() == buffer.capacity()) {
-      clr2.append_pixels(buffer.begin(), buffer.end());
-      buffer.clear();
-    }
-    if (j == update_frequency) {
-      const auto t1 = std::chrono::steady_clock::now();
-      const auto delta =
-          static_cast<double>(
-              std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count()) /
-          1000.0;
-      const auto bin1 = clr2.bins().at(first->bin1_id);
-      spdlog::info(FMT_STRING("[{} -> {}] processing {:ucsc} at {:.0f} pixels/s..."),
-                   clr1.bin_size(), resolution, bin1, double(update_frequency) / delta);
-      t0 = t1;
-      j = 0;
-    }
-    ++first;
+void zoomify_many(std::string_view in_uri, std::string_view out_path,
+                  const std::vector<std::uint32_t>& resolutions, bool copy_base_resolution,
+                  bool force) {
+  auto clr = cooler::File::open(in_uri);
+  auto mclr =
+      cooler::MultiResFile::create(out_path, cooler::File::open(in_uri).chromosomes(), force);
+
+  spdlog::info(FMT_STRING("coarsening cooler at {} {} times ({} -> {})"), clr.uri(),
+               resolutions.size(), clr.bin_size(), fmt::join(resolutions, " -> "));
+
+  if (copy_base_resolution) {
+    assert(resolutions.front() == clr.bin_size());
+    mclr.copy_resolution(clr);
+  } else {
+    assert(resolutions.size() >= 1);
+    zoomify_once(cooler::File::open(in_uri), mclr.init_resolution(resolutions[1]), resolutions[1]);
   }
-  if (!buffer.empty()) {
-    clr2.append_pixels(buffer.begin(), buffer.end());
+
+  for (std::size_t i = 1; i < resolutions.size(); ++i) {
+    mclr.create_resolution(resolutions[i]);
   }
 }
 
@@ -88,42 +81,9 @@ int zoomify_subcmd(const ZoomifyConfig& c) {
   const auto output_is_multires = c.copy_base_resolution || c.resolutions.size() > 2;
 
   if (output_is_multires) {
-    auto resolutions = c.resolutions;
-    if (!c.copy_base_resolution) {
-      resolutions.erase(resolutions.begin());
-    }
-    cooler::init_mcool(c.output_path, resolutions.begin(), resolutions.end(), c.force);
-  }
-
-  if (c.copy_base_resolution) {
-    spdlog::info(FMT_STRING("Copying {} resolution from {}"), c.resolutions.front(), c.input_uri);
-    cooler::utils::copy(c.input_uri, fmt::format(FMT_STRING("{}::/resolutions/{}"), c.output_path,
-                                                 c.resolutions.front()));
-  }
-
-  const auto input_resolution = c.resolutions.front();
-
-  std::vector<ThinPixel<std::int32_t>> buffer{500'000};
-
-  const internal::TmpDir tmpdir{};  // TODO update
-  for (std::size_t i = 1; i < c.resolutions.size(); ++i) {
-    const auto tgt_resolution = c.resolutions[i];
-    const auto base_resolution = compute_base_resolution(c.resolutions, tgt_resolution);
-
-    const auto tmpcooler = tmpdir() / fmt::format(FMT_STRING("{}.cool"), tgt_resolution);
-    const auto outcooler =
-        fmt::format(FMT_STRING("{}::/resolutions/{}"), c.output_path, tgt_resolution);
-    const auto incooler =
-        input_resolution == base_resolution
-            ? c.input_uri
-            : fmt::format(FMT_STRING("{}::/resolutions/{}"), c.output_path, base_resolution);
-
-    spdlog::info(FMT_STRING("Generating {} resolution from {} ({}x)"), tgt_resolution,
-                 base_resolution, tgt_resolution / base_resolution);
-
-    coarsen_cooler(incooler, tmpcooler.string(), tgt_resolution, buffer);
-    cooler::utils::copy(tmpcooler.string(), outcooler);
-    std::filesystem::remove(tmpcooler);
+    zoomify_many(c.input_uri, c.output_path, c.resolutions, c.copy_base_resolution, c.force);
+  } else {
+    zoomify_once(c.input_uri, c.output_path, c.resolutions.back(), c.force);
   }
 
   return 0;
