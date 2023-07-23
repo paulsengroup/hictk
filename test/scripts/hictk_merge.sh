b250d6dea45342cc33e7daf93f1e23c649aacf1a
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-07-23 12:39:28 +0200

    Add integration test for hictk merge

---
+++ test/scripts/hictk_merge.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -o pipefail
+set -u
+
+
+echo "#####################"
+echo "#### hictk merge ####"
+
+# readlink -f is not available on macos...
+function readlink_py {
+  set -eu
+  python3 -c 'import os, sys; print(os.path.realpath(sys.argv[1]))' "$1"
+}
+
+function check_files_exist {
+  set -eu
+  status=0
+  for f in "$@"; do
+    if [ ! -f "$f" ]; then
+      2>&1 echo "Unable to find test file \"$f\""
+      status=1
+    fi
+  done
+
+  return "$status"
+}
+
+function compare_coolers {
+  set -o pipefail
+  set -e
+
+  2>&1 echo "Comparing $1 with $2..."
+  if diff <(cooler dump -t chroms "$1") \
+          <(cooler dump -t chroms "$2") \
+     && \
+     diff <(cooler dump --join "$1") \
+          <(cooler dump --join "$2");
+  then
+    2>&1 echo "Files are identical"
+    return 0
+  else
+    2>&1 echo "Files differ"
+    return 1
+  fi
+}
+
+export function readlink_py
+
+status=0
+
+if [ $# -ne 1 ]; then
+  2>&1 echo "Usage: $0 path_to_hictk"
+  status=1
+fi
+
+hictk_bin="$1"
+
+data_dir="$(readlink_py "$(dirname "$0")/../data/")"
+script_dir="$(readlink_py "$(dirname "$0")")"
+
+input_cooler="$data_dir/integration_tests/4DNFIZ1ZVXC8.cool"
+
+export PATH="$PATH:$script_dir"
+
+if ! command -v cooler &> /dev/null; then
+  2>&1 echo "Unable to find cooler in your PATH"
+  status=1
+fi
+
+# Try to detect the error outlined below as early as possible:
+# https://github.com/open2c/cooler/pull/298
+cooler --help > /dev/null
+
+if [ $status -ne 0 ]; then
+  exit $status
+fi
+
+if ! check_files_exist "$input_cooler"; then
+  exit 1
+fi
+
+outdir="$(mktemp -d -t hictk-tmp-XXXXXXXXXX)"
+trap 'rm -rf -- "$outdir"' EXIT
+
+cooler merge "$outdir/expected.cool" "$input_cooler" "$input_cooler"
+"$hictk_bin" merge -o "$outdir/out.cool" "$input_cooler" "$input_cooler"
+
+if ! compare_coolers "$outdir/expected.cool" "$outdir/out.cool"; then
+  status=1
+fi
+
+if [ "$status" -eq 0 ]; then
+  printf '\n### PASS ###\n'
+else
+  printf '\n### FAIL ###\n'
+fi
+
+exit "$status"
