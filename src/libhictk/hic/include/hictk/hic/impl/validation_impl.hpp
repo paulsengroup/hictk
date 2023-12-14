971120d8e8b74912982fc8b321e2bd99fa8e580e
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-12-14 21:37:10 +0100

    Bugfix

---
+++ src/libhictk/hic/include/hictk/hic/impl/validation_impl.hpp
@@ -0,0 +1,19 @@
+// Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <algorithm>
+#include <cstdint>
+#include <filesystem>
+#include <vector>
+
+#include "hictk/hic/file_reader.hpp"
+
+namespace hictk::hic::utils {
+inline bool is_hic_file(const std::filesystem::path& path) {
+  return internal::HiCFileReader::checkMagicString(path.string());
+}
+
+}  // namespace hictk::hic::utils
