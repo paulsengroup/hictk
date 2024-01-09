48ef7844ca68ea02092cb1c1b2cf439fafad45e9
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-01-09 21:52:59 +0100

    Fix macOS builds

--- src/libhictk/hic/include/hictk/hic/impl/pixel_selector_impl.hpp
+++ src/libhictk/hic/include/hictk/hic/impl/pixel_selector_impl.hpp
@@ -124,7 +124,8 @@ inline ThinPixel<N> PixelSelector::transform_pixel(ThinPixel<float> pixel) const
       return float(_reader->avg());
     }
 
-    const auto i = std::min(bin2 - bin1, expected.size() - 1);
+    const auto i =
+        std::min(bin2 - bin1, conditional_static_cast<std::uint64_t>(expected.size() - 1));
     return float(expected[i]);
   }();
 
@@ -693,7 +694,8 @@ inline ThinPixel<N> PixelSelector::iterator<N>::transform_pixel(ThinPixel<float>
       return float(_reader->avg());
     }
 
-    const auto i = std::min(bin2 - bin1, expected.size() - 1);
+    const auto i =
+        std::min(bin2 - bin1, conditional_static_cast<std::uint64_t>(expected.size() - 1));
     return float(expected[i]);
   }();
 
