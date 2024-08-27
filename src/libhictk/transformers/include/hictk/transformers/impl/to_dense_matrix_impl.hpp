fd18dfae91b5e94e32e975cf8d84d310ba305268
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-08-27 09:46:11 +0200

    Bugfix
    [no ci]

--- src/libhictk/transformers/include/hictk/transformers/impl/to_dense_matrix_impl.hpp
+++ src/libhictk/transformers/include/hictk/transformers/impl/to_dense_matrix_impl.hpp
@@ -25,10 +25,10 @@ inline auto ToDenseMatrix<N, PixelSelector>::operator()() -> MatrixT {
   const auto mirror_matrix = _mirror && chrom1() == chrom2();
 
   if constexpr (internal::has_coord1_member_fx<PixelSelector>) {
-    if (chrom1() == chrom2() && _sel.coord2().bin2 > _sel.coord1().bin2) {
+    if (chrom1() == chrom2() && _sel.coord1() != _sel.coord2()) {
       auto coord3 = _sel.coord1();
       auto coord4 = _sel.coord2();
-      coord3.bin1 = std::min(coord3.bin1, coord4.bin1);  // probably unnecessary
+      coord3.bin1 = std::min(coord3.bin1, coord4.bin1);
       coord3.bin2 = std::max(coord3.bin2, coord4.bin2);
       coord4 = coord3;
 
