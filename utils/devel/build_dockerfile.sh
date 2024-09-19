9a01ef13588053300b2ff4852182b576d66221f8
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-09-19 13:02:00 +0200

    Bump compiler used to build Dockerfile to clang-19

--- utils/devel/build_dockerfile.sh
+++ utils/devel/build_dockerfile.sh
@@ -9,6 +9,14 @@ set -u
 set -o pipefail
 
 IMAGE_NAME='hictk'
+COMPILER_NAME='clang'
+COMPILER_VERSION='19'
+C_COMPILER="$COMPILER_NAME-$COMPILER_VERSION"
+if [[ "$COMPILER_NAME" == gcc ]]; then
+  CXX_COMPILER="g++$COMPILER_VERSION"
+else
+  CXX_COMPILER="clang++$COMPILER_VERSION"
+fi
 
 GIT_HASH="$(git rev-parse HEAD)"
 GIT_SHORT_HASH="$(git rev-parse --short HEAD)"
@@ -34,20 +42,20 @@ fi
 
 2>&1 echo "Building \"$IMAGE_NAME:$IMAGE_TAG\"..."
 
-sudo docker pull docker.io/library/ubuntu:22.04
-FINAL_BASE_IMAGE_DIGEST="$(sudo docker inspect --format='{{index .RepoDigests 0}}' docker.io/library/ubuntu:22.04 | grep -o '[[:alnum:]:]\+$')"
+sudo docker pull docker.io/library/ubuntu:24.04
+FINAL_BASE_IMAGE_DIGEST="$(sudo docker inspect --format='{{index .RepoDigests 0}}' docker.io/library/ubuntu:24.04 | grep -o '[[:alnum:]:]\+$')"
 
-BUILD_BASE_IMAGE='ghcr.io/paulsengroup/ci-docker-images/ubuntu-22.04-cxx-clang-17:latest'
+BUILD_BASE_IMAGE="ghcr.io/paulsengroup/ci-docker-images/ubuntu-24.04-cxx-$C_COMPILER:latest"
 
 sudo docker pull "$BUILD_BASE_IMAGE"
 
-sudo docker buildx build --platform linux/amd64,linux/arm64 \
+sudo docker buildx build --platform linux/amd64 \
   --build-arg "BUILD_BASE_IMAGE=$BUILD_BASE_IMAGE" \
   --build-arg "FINAL_BASE_IMAGE=docker.io/library/ubuntu" \
-  --build-arg "FINAL_BASE_IMAGE_TAG=22.04" \
+  --build-arg "FINAL_BASE_IMAGE_TAG=24.04" \
   --build-arg "FINAL_BASE_IMAGE_DIGEST=$FINAL_BASE_IMAGE_DIGEST" \
-  --build-arg "C_COMPILER=clang-17" \
-  --build-arg "CXX_COMPILER=clang++-17" \
+  --build-arg "C_COMPILER=$C_COMPILER" \
+  --build-arg "CXX_COMPILER=$CXX_COMPILER" \
   --build-arg "GIT_HASH=$GIT_HASH" \
   --build-arg "GIT_SHORT_HASH=$GIT_SHORT_HASH" \
   --build-arg "GIT_TAG=$GIT_TAG" \
