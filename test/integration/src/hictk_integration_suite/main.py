b9d411915c9389fe9b7ea418744a63081906c393
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-09-20 14:37:58 +0200

    Start porting integration tests for hictk load

--- test/integration/src/hictk_integration_suite/main.py
+++ test/integration/src/hictk_integration_suite/main.py
@@ -37,6 +37,7 @@ def get_test_names(include_all: bool = True) -> List[str]:
         "convert",
         "dump",
         "fix-mcool",
+        "load",
         "merge",
         "metadata",
         "rename-chromosomes",
