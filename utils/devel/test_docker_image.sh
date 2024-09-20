c4c4d58f9c836fc88db517a5ecaf8ddccb2b5ea4
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-09-20 19:14:17 +0200

    Update script to test Docker image

--- utils/devel/test_docker_image.sh
+++ utils/devel/test_docker_image.sh
@@ -20,23 +20,30 @@ trap "rm -rf '$tmpdir'" EXIT
 cat > "$tmpdir/runme.sh" <<- 'EOM'
 
 set -eu
-cd /tmp/hictk
-
-apt-get update
-apt-get install -y --no-install-recommends \
-  curl \
-  python3 \
-  xz-utils
-
-tar -xf test/data/hictk_test_data.tar.zst
-
-tmpdir="$(mktemp -d)"
-trap "rm -rf '$tmpdir'" EXIT
 
 whereis -b hictk
 
-test/scripts/hictk_convert_hic2cool.sh "$(which hictk)"
-test/scripts/hictk_convert_cool2hic.sh "$(which hictk)"
+apt-get update
+apt-get install -q -y --no-install-recommends \
+  python3 \
+  python3-pip \
+  python3-venv \
+  tar \
+  which \
+  zstd
+
+cd /tmp/hictk
+
+tar -xf test/data/hictk_test_data.tar.zst
+
+python3 -m venv venv --upgrade
+venv/bin/pip install test/integration
+
+venv/bin/hictk_integration_suite \
+  "$(which hictk)" \
+  test/integration/config.toml \
+  --data-dir test/data \
+  --threads "$(nproc)"
 
 EOM
 
@@ -44,7 +51,7 @@ chmod 755 "$tmpdir/runme.sh"
 
 sudo docker run --rm --entrypoint=/bin/bash \
   -v "$tmpdir/runme.sh:/tmp/runme.sh:ro" \
-  -v "$PWD/test/scripts:/tmp/hictk/test/scripts:ro" \
+  -v "$PWD/test/integration:/tmp/hictk/test/integration:ro" \
   -v "$PWD/test/data/hictk_test_data.tar.zst:/tmp/hictk/test/data/hictk_test_data.tar.zst:ro" \
   "$IMG" \
   /tmp/runme.sh
