cbc768eefdbdbc8d5d6b11445b5be60c9684e49c
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-09-20 14:44:11 +0200

    Update test dataset [no ci]

--- cmake/FetchTestDataset.cmake
+++ cmake/FetchTestDataset.cmake
@@ -4,14 +4,14 @@
 
 # cmake-format: off
 file(
-  DOWNLOAD https://zenodo.org/records/13796876/files/hictk_test_data.tar.xz?download=1
-  EXPECTED_HASH SHA256=ef24c42d00f9d3472dcf5a66df2588671bc17d019418b33812dfbdb532396d5f
-  "${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.xz")
+  DOWNLOAD https://zenodo.org/records/13798772/files/hictk_test_data.tar.zst?download=1
+  EXPECTED_HASH SHA256=0a92335d6adf2991609dea005c77991f4a4c8df0405a6c425eb7176945b7443e
+  "${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.zst")
 # cmake-format: on
 
 file(
   ARCHIVE_EXTRACT
   INPUT
-  "${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.xz"
+  "${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.zst"
   DESTINATION
   "${PROJECT_SOURCE_DIR}")
