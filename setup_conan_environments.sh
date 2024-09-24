7b427586533998d0d04b3d2356d0e601a614466d
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-09-24 19:30:47 +0200

    Fix setup_conan_environments.sh

--- setup_conan_environments.sh
+++ setup_conan_environments.sh
@@ -38,7 +38,7 @@ for compiler in gcc clang; do
       -pr "$profile"  \
       -s compiler.cppstd=17 \
       -s build_type="$build_type" \
-      --output-folder="$outdir" \
+      --output-folder="$outdir/static" \
       "$conanfile"
 
      conan install \
@@ -47,8 +47,8 @@ for compiler in gcc clang; do
        -pr "$profile"  \
        -s compiler.cppstd=17 \
        -s build_type="$build_type" \
-       -o shared=True \
-       --output-folder="$outdir" \
+       -o '*/*:shared=True' \
+       --output-folder="$outdir/shared" \
        "$conanfile"
   done
 done
