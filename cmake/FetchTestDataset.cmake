e5c4e3bc73d5e263f1bb60a1ddafce526aba209e
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-09-27 19:18:49 +0200

    Add integration test cases covering the new flags

--- cmake/FetchTestDataset.cmake
+++ cmake/FetchTestDataset.cmake
@@ -4,8 +4,8 @@
 
 # cmake-format: off
 file(
-  DOWNLOAD https://zenodo.org/records/13825195/files/hictk_test_data.tar.zst?download=1
-  EXPECTED_HASH SHA256=e5a93842a3d068d752f28da8cc0b9ba90064ad81ccfaa811fd68e96c5af221dc
+  DOWNLOAD https://zenodo.org/records/13851354/files/hictk_test_data.tar.zst?download=1
+  EXPECTED_HASH SHA256=55071172638948112a69a43ebfd07b0b9b830cc5e1bfef87b05b586d228ab1bd
   "${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.zst"
 )
 # cmake-format: on
