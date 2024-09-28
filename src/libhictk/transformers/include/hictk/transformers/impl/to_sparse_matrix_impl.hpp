2a4bdc3ad3f8919bfa504a0895215e69033ad3f3
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-09-28 13:04:37 +0200

    Make constructors for To*Matrix transformers more flexible

--- src/libhictk/transformers/include/hictk/transformers/impl/to_sparse_matrix_impl.hpp
+++ src/libhictk/transformers/include/hictk/transformers/impl/to_sparse_matrix_impl.hpp
@@ -6,7 +6,9 @@
 
 #include <Eigen/SparseCore>
 #include <algorithm>
+#include <cassert>
 #include <cstdint>
+#include <memory>
 #include <utility>
 
 #include "hictk/pixel.hpp"
@@ -15,9 +17,17 @@
 namespace hictk::transformers {
 
 template <typename N, typename PixelSelector>
-inline ToSparseMatrix<N, PixelSelector>::ToSparseMatrix(PixelSelector&& sel, [[maybe_unused]] N n,
+inline ToSparseMatrix<N, PixelSelector>::ToSparseMatrix(PixelSelector sel, [[maybe_unused]] N n,
                                                         QuerySpan span)
+    : ToSparseMatrix(std::make_shared<const PixelSelector>(std::move(sel)), n, span) {}
+
+template <typename N, typename PixelSelector>
+inline ToSparseMatrix<N, PixelSelector>::ToSparseMatrix(std::shared_ptr<const PixelSelector> sel,
+                                                        [[maybe_unused]] N n, QuerySpan span)
     : _sel(std::move(sel)), _span(span) {
+  if (!_sel) {
+    throw std::runtime_error("hictk::transformers::ToSparseMatrix(): sel cannot be null");
+  }
   if (chrom1() != chrom2() && span == QuerySpan::lower_triangle) {
     throw std::runtime_error(
         "hictk::transformers::ToSparseMatrix(): invalid parameters. Trans queries do not support "
@@ -28,6 +38,7 @@ inline ToSparseMatrix<N, PixelSelector>::ToSparseMatrix(PixelSelector&& sel, [[m
 
 template <typename N, typename PixelSelector>
 inline auto ToSparseMatrix<N, PixelSelector>::operator()() -> MatrixT {
+  assert(!!_sel);
   const auto populate_lower_triangle =
       _span == QuerySpan::lower_triangle || _span == QuerySpan::full;
   const auto populate_upper_triangle =
@@ -39,14 +50,14 @@ inline auto ToSparseMatrix<N, PixelSelector>::operator()() -> MatrixT {
 
   MatrixT matrix(num_rows(), num_cols());
   if constexpr (internal::has_coord1_member_fx<PixelSelector>) {
-    if (chrom1() == chrom2() && _sel.coord1() != _sel.coord2()) {
-      auto coord3 = _sel.coord1();
-      auto coord4 = _sel.coord2();
+    if (chrom1() == chrom2() && _sel->coord1() != _sel->coord2()) {
+      auto coord3 = _sel->coord1();
+      auto coord4 = _sel->coord2();
       coord3.bin1 = std::min(coord3.bin1, coord4.bin1);
       coord3.bin2 = std::max(coord3.bin2, coord4.bin2);
       coord4 = coord3;
 
-      internal::fill_matrix<N>(_sel.fetch(coord3, coord4), matrix, matrix.rows(), matrix.cols(),
+      internal::fill_matrix<N>(_sel->fetch(coord3, coord4), matrix, matrix.rows(), matrix.cols(),
                                row_offset(), col_offset(), populate_lower_triangle,
                                populate_upper_triangle, matrix_setter);
       matrix.makeCompressed();
@@ -54,7 +65,7 @@ inline auto ToSparseMatrix<N, PixelSelector>::operator()() -> MatrixT {
     }
   }
 
-  internal::fill_matrix<N>(_sel, matrix, matrix.rows(), matrix.cols(), row_offset(), col_offset(),
+  internal::fill_matrix<N>(*_sel, matrix, matrix.rows(), matrix.cols(), row_offset(), col_offset(),
                            populate_lower_triangle, populate_upper_triangle, matrix_setter);
   matrix.makeCompressed();
   return matrix;
@@ -62,16 +73,18 @@ inline auto ToSparseMatrix<N, PixelSelector>::operator()() -> MatrixT {
 
 template <typename N, typename PixelSelector>
 inline std::string_view ToSparseMatrix<N, PixelSelector>::chrom1() const noexcept {
+  assert(!!_sel);
   if constexpr (internal::has_coord1_member_fx<PixelSelector>) {
-    return _sel.coord1().bin1.chrom().name();
+    return _sel->coord1().bin1.chrom().name();
   }
   return "all";
 }
 
 template <typename N, typename PixelSelector>
 inline std::string_view ToSparseMatrix<N, PixelSelector>::chrom2() const noexcept {
+  assert(!!_sel);
   if constexpr (internal::has_coord1_member_fx<PixelSelector>) {
-    return _sel.coord2().bin1.chrom().name();
+    return _sel->coord2().bin1.chrom().name();
   }
   return "all";
 }
@@ -93,32 +106,36 @@ inline std::int64_t ToSparseMatrix<N, PixelSelector>::num_bins(const PixelCoordi
 
 template <typename N, typename PixelSelector>
 inline std::int64_t ToSparseMatrix<N, PixelSelector>::num_rows() const noexcept {
+  assert(!!_sel);
   if constexpr (internal::has_coord1_member_fx<PixelSelector>) {
-    return num_bins(_sel.coord1(), _sel.bins());
+    return num_bins(_sel->coord1(), _sel->bins());
   }
-  return static_cast<std::int64_t>(_sel.bins().size());
+  return static_cast<std::int64_t>(_sel->bins().size());
 }
 
 template <typename N, typename PixelSelector>
 inline std::int64_t ToSparseMatrix<N, PixelSelector>::num_cols() const noexcept {
+  assert(!!_sel);
   if constexpr (internal::has_coord1_member_fx<PixelSelector>) {
-    return num_bins(_sel.coord2(), _sel.bins());
+    return num_bins(_sel->coord2(), _sel->bins());
   }
-  return static_cast<std::int64_t>(_sel.bins().size());
+  return static_cast<std::int64_t>(_sel->bins().size());
 }
 
 template <typename N, typename PixelSelector>
 inline std::int64_t ToSparseMatrix<N, PixelSelector>::row_offset() const noexcept {
+  assert(!!_sel);
   if constexpr (internal::has_coord1_member_fx<PixelSelector>) {
-    return offset(_sel.coord1());
+    return offset(_sel->coord1());
   }
   return 0;
 }
 
 template <typename N, typename PixelSelector>
 inline std::int64_t ToSparseMatrix<N, PixelSelector>::col_offset() const noexcept {
+  assert(!!_sel);
   if constexpr (internal::has_coord1_member_fx<PixelSelector>) {
-    return offset(_sel.coord2());
+    return offset(_sel->coord2());
   }
   return 0;
 }
@@ -132,6 +149,7 @@ inline std::int64_t ToSparseMatrix<N, PixelSelector>::offset(
 
 template <typename N, typename PixelSelector>
 inline void ToSparseMatrix<N, PixelSelector>::validate_dtype() const {
+  assert(!!_sel);
   if constexpr (std::is_floating_point_v<N>) {
     return;
   }
@@ -141,11 +159,11 @@ inline void ToSparseMatrix<N, PixelSelector>::validate_dtype() const {
       "type when fetching normalized interactions.";
 
   if constexpr (internal::has_weights_member_fx<PixelSelector>) {
-    if (!_sel.weights().is_vector_of_ones()) {
+    if (!_sel->weights().is_vector_of_ones()) {
       throw std::runtime_error(msg);
     }
   } else {
-    if (!_sel.weights1().is_vector_of_ones() || !_sel.weights2().is_vector_of_ones()) {
+    if (!_sel->weights1().is_vector_of_ones() || !_sel->weights2().is_vector_of_ones()) {
       throw std::runtime_error(msg);
     }
   }
