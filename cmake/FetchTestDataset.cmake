d10ff17e09986587ef646f0d78f7c2dc7d13a7f4
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-10-08 19:10:15 +0200

    Format project using gersemi [no ci]

--- cmake/FetchTestDataset.cmake
+++ cmake/FetchTestDataset.cmake
@@ -2,17 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 
-# cmake-format: off
+# gersemi: off
 file(
   DOWNLOAD https://zenodo.org/records/13851354/files/hictk_test_data.tar.zst?download=1
   EXPECTED_HASH SHA256=55071172638948112a69a43ebfd07b0b9b830cc5e1bfef87b05b586d228ab1bd
   "${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.zst"
 )
-# cmake-format: on
+# gersemi: on
 
 file(
   ARCHIVE_EXTRACT
-  INPUT
-  "${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.zst"
-  DESTINATION
-  "${PROJECT_SOURCE_DIR}")
+  INPUT "${PROJECT_SOURCE_DIR}/test/data/hictk_test_data.tar.zst"
+  DESTINATION "${PROJECT_SOURCE_DIR}"
+)
