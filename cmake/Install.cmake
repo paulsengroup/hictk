1b1c6c6be109f7ba5f81ad864e7ae5d4195aea70
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-04-04 17:31:17 +0200

    Move hictk/type_traits.hpp into its own target

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -12,6 +12,7 @@ set(
   common/default_libdeflate
   common/default_zstd
   common/genomic_units
+  common/type_traits
   cooler
   expected_values_aggregator
   file
@@ -67,6 +68,7 @@ install(
     hictk_internal_common_default_delete_libdeflate
     hictk_internal_common_default_delete_zstd
     hictk_internal_common_genomic_units
+    hictk_internal_common_type_traits
     hictk_internal_cooler
     hictk_internal_expected_values_aggregator
     hictk_internal_file
