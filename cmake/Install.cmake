73327acb3f809100eedb716b5ae61280cfb11d58
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-04-04 17:45:59 +0200

    Move hictk/tmpdir.hpp into its own target

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -12,6 +12,7 @@ set(
   common/default_libdeflate
   common/default_zstd
   common/genomic_units
+  common/tmpdir
   common/type_pretty_printer
   common/type_traits
   cooler
@@ -69,6 +70,7 @@ install(
     hictk_internal_common_default_delete_libdeflate
     hictk_internal_common_default_delete_zstd
     hictk_internal_common_genomic_units
+    hictk_internal_common_tmpdir
     hictk_internal_common_type_pretty_printer
     hictk_internal_common_type_traits
     hictk_internal_cooler
