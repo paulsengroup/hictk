62cfea145c1f14519101ae1579ff1cd10b36b372
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2025-10-08 15:46:02 +0200

    Make sure to use the correct count type when pre-allocating sparse matrices

--- src/libhictk/transformers/include/hictk/transformers/impl/to_sparse_matrix_impl.hpp
+++ src/libhictk/transformers/include/hictk/transformers/impl/to_sparse_matrix_impl.hpp
@@ -413,10 +413,12 @@ inline auto ToSparseMatrix<N, PixelSelector>::pre_allocate_matrix(
   const auto selector_is_symmetric_upper = internal::selector_is_symmetric_upper(sel);
 
   std::vector<std::int64_t> nnzs(static_cast<std::size_t>(num_rows()), 0);
-  internal::fill_matrix(
-      sel.template begin<std::int_fast8_t>(), sel.template end<std::int_fast8_t>(),
-      _diagonal_band_width, selector_is_symmetric_upper, nnzs, nnzs, num_rows(), num_cols(),
-      row_offset(), col_offset(), populate_lower_triangle, populate_upper_triangle, setter);
+
+  using M = std::conditional_t<std::is_floating_point_v<N>, float, std::int_fast8_t>;
+  internal::fill_matrix(sel.template begin<M>(), sel.template end<M>(), _diagonal_band_width,
+                        selector_is_symmetric_upper, nnzs, nnzs, num_rows(), num_cols(),
+                        row_offset(), col_offset(), populate_lower_triangle,
+                        populate_upper_triangle, setter);
 
   MatrixT matrix(num_rows(), num_cols());
   matrix.reserve(nnzs);
